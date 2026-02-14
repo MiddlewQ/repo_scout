2d2a559bc778022fbd31c93a341b116f504fea63
Viktor Bergmark <viktor.bergmark@hotmail.com>
2026-02-14 18:58:27 +0100

    update: dir/file type, handle filetypes without ext

--- repo_scout/scan/scan_repo.py
+++ repo_scout/scan/scan_repo.py
@@ -4,16 +4,26 @@ from typing import Any
 from .file_type import file_ext_to_file_type
 from .tool_response import response_ok, response_error
 
-def create_file_info(entry: os.DirEntry, root: str) -> dict:
-    stats = entry.stat(follow_symlinks=False)
-    _, ext = os.path.splitext(entry.name)
+def create_dir_info(entry: os.DirEntry, root: str) -> dict:
     relative_path = os.path.relpath(entry.path, root)
     return {
         "path": relative_path,
+        "kind": "dir",
+    }
+
+def create_file_info(entry: os.DirEntry, root: str) -> dict:
+    stats = entry.stat(follow_symlinks=False)
+    file_name, ext = os.path.splitext(entry.name)
+    if ext == '':
+        ext = file_name
+    relative_path = os.path.relpath(entry.path, root)
+    return {
+        "path": relative_path,
+        "kind": "file",
         "size": stats.st_size,
-        "last_modfied": stats.st_mtime,
+        "last_modified": stats.st_mtime,
         "hash": None,
-        "type": file_ext_to_file_type(ext)
+        "file_type": file_ext_to_file_type(ext).value
     }
 
 def fs_tree(root: str = ".",
@@ -41,10 +51,11 @@ def fs_tree_helper(curr: str,
         with os.scandir(curr) as it:
             for entry in it:
                 name = entry.name
+                if name in ignore:
+                    continue
                 if entry.is_dir(follow_symlinks=False):
-                    if name in ignore:
-                        continue
-                    out[name] = fs_tree_helper(entry.path, root, ignore, depth - 1)
+                    out[name] = create_dir_info(entry, root)
+                    out[name]["children"] = fs_tree_helper(entry.path, root, ignore, depth - 1)
                 elif entry.is_file(follow_symlinks=False):
                     out[name] = create_file_info(entry, root)
     except OSError as e:
