12421e81bd05d8a2f6de7af26c562f409d9f8a1e
Viktor Bergmark <viktor.bergmark@hotmail.com>
2026-02-17 22:54:08 +0100

    refactor: get_nodes -> nodes

--- tests/test_db_insert.py
+++ tests/test_db_insert.py
@@ -1,6 +1,6 @@
 from repo_scout.database.init_db import init_db
 from repo_scout.database.scan_runs import begin_scan_run
-from repo_scout.database.nodes import walk_and_insert, get_nodes
+from repo_scout.database.nodes import walk_and_insert, nodes
 from repo_scout.scan.scan_repo import fs_tree
 
 filesystem = fs_tree(".", ignore=set(["__pycache__", ".git", ".venv"]))
@@ -11,7 +11,7 @@ try:
         run_id = begin_scan_run(conn)
         walk_and_insert(conn, filesystem, run_id)
 
-    nodes = get_nodes(conn)
+    nodes = nodes(conn)
     print("Node data:")
     for row in nodes:
         print(row)
