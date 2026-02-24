092f7dc2da73c8c5b4d7503b8bd3c90f13b5f588
Viktor Bergmark <viktor.bergmark@outlook.com>
2026-02-24 10:31:03 +0100

    add: print args for debug

--- repo_scout/main.py
+++ repo_scout/main.py
@@ -5,13 +5,13 @@ def main():
     parser = init_parser()
     args = parser.parse_args()
 
-    if args.verbose:
-        print(args)
-
     if args.command is None:
         parser.print_help()
         raise SystemExit(0)
 
+    if args.verbose:
+        print(f"Running {args.command}")
+        print(args)
     args.func(args)
 
 if __name__ == "__main__":
