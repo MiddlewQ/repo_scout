afc6fa7253ab0ce4759a010a2c8c2e8fe96626a5
Viktor Bergmark <viktor.bergmark@outlook.com>
2026-02-24 10:31:58 +0100

    add: read gitignore for default ignore list

--- repo_scout/repo_root.py
+++ repo_scout/repo_root.py
@@ -1,4 +1,4 @@
-import os
+import os, pathspec
 
 def find_repo_root(curr_dir: str | None = None) -> str | None:
     curr_dir = os.path.abspath(curr_dir or os.getcwd())
@@ -28,3 +28,25 @@ def resolve_repo_root(repo_arg: str | None = None):
 
 def db_path_to_root(repo_root: str) -> str:
     return os.path.join(repo_root, ".repo_scout", "index.sqlite")
+
+def load_gitignore(repo_root: str, gitignore_path: str | None = None, verbose: bool = False) -> set[str]:
+    
+    if gitignore_path is None:
+        gitignore_path = os.path.join(repo_root, ".gitignore")
+
+    if not os.path.exists(gitignore_path):
+        if verbose:
+            print("No gitignore found.")
+        return set()
+
+    ignore_list = set()
+    try: 
+        with open(gitignore_path, mode="r", encoding="utf-8")as f:
+            content = f.read()
+            for line in content.splitlines():
+                ignore_list.add(line)                
+    except OSError as e:
+        print(e)
+        raise
+
+    return ignore_list
